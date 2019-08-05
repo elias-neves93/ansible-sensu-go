d91fa039a79e784c6eeb75e9a3f8afec5b02b3db
Elias Neves <elias.neves@netshoes.com>
2019-08-05 12:42:57 -0300

    Fixed error lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,11 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nginx_config(host):
-    config = host.run("sudo systemctl status nginx")
+def test_sensu_backend_config(host):
+    config = host.run("sudo systemctl status sensu-backend")
     assert config.rc == 0
 
 
-def test_nginx_local_port(host):
-    local_port = host.socket("tcp://0.0.0.0:80")
+def test_sensu_local_port(host):
+    local_port = host.socket("tcp://0.0.0.0:3000")
     assert local_port.is_listening
