978ece7229347ad0c9b08ba454bd998b2fd26be0
Elias Neves <elias.neves@netshoes.com>
2019-08-25 13:56:17 -0300

    Fixed assets and checks.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-![build status](https://gitlab.com/elias-neves93/ansible-sensu-go/badges/master/build.svg)
+[![Build Status](https://travis-ci.org/elias-neves93/ansible-sensu-go.svg?branch=master)](https://travis-ci.org/elias-neves93/ansible-sensu-go)
+
 # ansible-sensu-go
 Ansible Role to Sensu GO
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,3 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_sensu_backend_config(host):
     config = host.run("sudo systemctl status sensu-backend")
     assert config.rc == 0
+
+def test_sensu_backend_local_port(host):
+    local_port = host.socket("tcp://127.0.0.1:3000")
+    assert local_port.is_listening
+
+
+def test_sensu_backend_and_sensu_agent_running_and_enabled(host):
+    sensu_backend_service = host.service("sensu-backend")
+    assert sensu_backend_service.is_running
+    assert sensu_backend_service.is_enabled
+
+    sensu_agent_service = host.service("sensu-agent")
+    assert sensu_agent_service.is_running
+    assert sensu_agent_service.is_enabled
